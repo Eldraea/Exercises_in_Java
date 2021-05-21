751a64702330f4ada4e71dc672e132cf52a6a57a
Kimberly <kjacques709@gmail.com>
2021-05-21 10:27:41 +0200

    Second program creation and implementation: defining a variable and printing it

---
+++ Variables/src/variables_exercise.java
@@ -0,0 +1,21 @@
+/*
+ variables_exercise.java
+ ----------------------------
+ Created by Eldr@e@
+ ----------------------------
+ This program is an introduction to the wonderful world of variables and data types
+ -----------------------------
+ Created on: 21/05/2021
+ -----------------------------
+ Last Update on: 21/05/2021
+
+ */
+
+
+public class variables_exercise {
+    public static void main(String[] args){
+
+        int myFirstNumber = (5 + 10) + (2 *10 );
+        System.out.println(myFirstNumber);
+    }
+}
