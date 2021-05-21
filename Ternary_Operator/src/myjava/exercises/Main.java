ed37d4b79ed16219a1dfab4f0de14c8e3c5a36bc
Kimberly <kjacques709@gmail.com>
2021-05-21 23:11:40 +0200

    Eighteenth Program: Introduction to Ternary Operator

---
+++ Ternary_Operator/src/myjava/exercises/Main.java
@@ -0,0 +1,27 @@
+/*
+main.java
+--------------------------------
+Created by Eldr@e@
+--------------------------------
+Introduction to Ternary Operator
+---------------------------------
+Created on : 21/05/2021
+---------------------------------
+Last Update on: 21/05/2021
+----------------------------------
+ */
+
+package myjava.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isCar = false;
+        boolean wasCar = isCar? true : false;
+        if(wasCar)
+        {
+            System.out.println("wasCar is true");
+        }
+    }
+}
