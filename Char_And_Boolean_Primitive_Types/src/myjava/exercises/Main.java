d0decff29f8d0b6252cf17328eb7a3ddb88548a7
Kimberly <kjacques709@gmail.com>
2021-05-21 13:46:19 +0200

    Tenth Program: Char and Boolean Data Types Introduction

---
+++ Char_And_Boolean_Primitive_Types/src/myjava/exercises/Main.java
@@ -0,0 +1,32 @@
+/*
+main.java
+---------------------------------------------------
+Created by Eldr@e@
+---------------------------------------------------
+Char and Boolean Primitive Data Types Introduction
+---------------------------------------------------
+Created on: 21/05/2021
+---------------------------------------------------
+Last Update on : 21/05/2021
+---------------------------------------------------
+ */
+
+package myjava.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar ='\u00A9';
+        System.out.println(myCopyrightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
