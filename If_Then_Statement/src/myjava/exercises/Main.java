43fc2681e2439b3e61be9168fc2c5d1fd1832274
Kimberly <kjacques709@gmail.com>
2021-05-21 21:10:59 +0200

    Fourteenth Program: Introduction to If-Then Statement

---
+++ If_Then_Statement/src/myjava/exercises/Main.java
@@ -0,0 +1,26 @@
+/*
+main.java
+----------------------------------
+Created by Eldr@e@
+----------------------------------
+Introduction to If-Then Statement
+----------------------------------
+Created on: 21/05/2021
+----------------------------------
+Last Update on: 21/5/2021
+----------------------------------
+ */
+
+package myjava.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("Person 1: It's not an alien you fool!!");
+            System.out.println("Person 2: Oof I am so scared of aliens...");
+        }
+    }
+}
