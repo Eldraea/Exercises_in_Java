d0bef57ee48d7537b1aa46f8754bf145820164bd
Kimberly <kjacques709@gmail.com>
2021-05-22 08:53:43 +0200

    Twenty-First Program: A program that explains the utility of  whitespaces and indentation for statements

---
+++ Statements_Whitespaces_And_Indentation/src/myjava/exercises/Main.java
@@ -0,0 +1,38 @@
+/*
+main.java
+---------------------------------------------------------------------------------
+Created by Eldr@e@
+----------------------------------------------------------------------------------
+A program that explains the utility of  whitespaces and indentation for statements
+----------------------------------------------------------------------------------
+Created on: 22/05/2021
+-----------------------------------------------------------------------------------
+Last Update on: 22/05/2021
+-----------------------------------------------------------------------------------
+ */
+
+package myjava.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int                                           myVariable
+                =50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is " +
+                " another " +
+                "still more.");
+
+        int anotherVariable = 50; myVariable--;System.out.println("This is another one !!");
+
+        //Reading this code can be a little bit difficult so we use whitespaces, indentation for our statements
+
+
+
+
+
+    }
+}
