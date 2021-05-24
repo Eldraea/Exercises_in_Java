1e1f9b2e23e119e18981b5454d026c166cf023cb
Kimberly <kjacques709@gmail.com>
2021-05-24 14:38:33 +0200

    Fifty-Fourth Program: The Perfect Number Challenge

---
+++ Perfect_Number/src/myjava/exercises/Main.java
@@ -0,0 +1,26 @@
+/*
+Main.java
+------------------------------------------------------------------------------------------------------------------------
+Created by Eldr@e@
+------------------------------------------------------------------------------------------------------------------------
+Main class that tests the PerfectNumber class
+------------------------------------------------------------------------------------------------------------------------
+Created on: 24/05/2021
+------------------------------------------------------------------------------------------------------------------------
+Last Update on: 24/05/2021
+------------------------------------------------------------------------------------------------------------------------
+ */
+
+
+package myjava.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(PerfectNumber.isPerfectNumber(6));
+        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println(PerfectNumber.isPerfectNumber(5));
+        System.out.println(PerfectNumber.isPerfectNumber(-1));
+    }
+}
