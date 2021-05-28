31707b5fd5501a91e76dc70488c7c339fc0d20db
Kimberly <kjacques709@gmail.com>
2021-05-28 19:53:54 +0200

    Ninety-Third Program : Introduction to Inner classes

---
+++ Inner_Class/src/myjava/exercises/Button.java
@@ -0,0 +1,32 @@
+package myjava.exercises;
+
+public class Button {
+
+    private String title;
+    private OnClickListener onClickLister;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickLister(OnClickListener onClickListener)
+    {
+        this.onClickLister = onClickListener;
+    }
+
+    public void onClick()
+    {
+        this.onClickLister.onClick(this.title);
+
+    }
+
+    public interface OnClickListener
+    {
+
+        void onClick(String title);
+    }
+}
