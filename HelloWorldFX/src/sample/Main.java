09f70897641d78f38b6ba974dc04fb239de796b1
Kimberly <kjacques709@gmail.com>
2021-06-22 08:27:35 +0200

    Hundred-Fourth Program: Introduction to JavaFX

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -10,22 +10,52 @@ Created on 20/06/2021
 Last Update on: 20/06/2021
 ------------------------------------------------------------------------------------------------------------------------
  */
+/*
+Main.java
+------------------------------------------------------------------------------------------------------------------------
+Created by Eldr@e@
+------------------------------------------------------------------------------------------------------------------------
+Introduction to JavaFX
+------------------------------------------------------------------------------------------------------------------------
+Created on 22/06/2021
+------------------------------------------------------------------------------------------------------------------------
+Last Update on: 22/06/2021
+------------------------------------------------------------------------------------------------------------------------
+ */
 
 package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.OverrunStyle;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+        Label greetings = new Label("Welcome to JavaFX!!");
+        greetings.setTextFill(Color.rgb(43,84,86));
+        greetings.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+        root.getChildren().add(greetings);
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
